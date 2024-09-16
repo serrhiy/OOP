0963129415191f87c0994e8a9926705a7909b870
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-09-16 15:50:28 +0300

    finish lab1

--- lab1/src/build.sh
+++ lab1/src/build.sh
@@ -1,2 +1,3 @@
 javac --module-path /home/serhii/programming/code/java/libs/javafx/lib --add-modules=javafx.controls,javafx.fxml Main.java ./controllers/MenuController.java ./controllers/Work1Controller.java ./controllers/Work2Controller.java
 java --module-path /home/serhii/programming/code/java/libs/javafx/lib --add-modules=javafx.controls,javafx.fxml Main
+rm *.class ./controllers/*.class
